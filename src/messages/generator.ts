eaa6cb90434771cc3b23301e8f27634c923cce76
Omar Mohammad <omarmo@cloudflare.com>
2023-06-01 17:29:29 +0100

    Add reminder for kudos

--- src/messages/generator.ts
+++ src/messages/generator.ts
@@ -8,18 +8,24 @@ export const getGreeting = () => {
 };
 
 export const generateMessage = (options: any, env: Env) => {
-  const date = new Intl.DateTimeFormat("en-US", {
+  const now = Date.now();
+
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
     year: "numeric",
     month: "short",
     day: "numeric",
-  }).format(Date.now());
+  }).format(now);
+
+  const dayOfWeek = new Intl.DateTimeFormat("en-US", {
+    weekday: "long",
+  }).format(now);
 
   const greeting = getGreeting();
 
   return {
     text: `
 *_Good morning!_*
-*_Daily update thread [ ${date} ]_*
+*_Daily update thread [ ${formattedDate} ]_*
     
 -- Update template: feel free to copy-paste --
 
@@ -32,6 +38,10 @@ answer
 *What questions I have open, and to whom?*
 answer
 
+${
+  dayOfWeek === "Friday" &&
+  "Don't forget to send your kudos and Aperitivo ideas!"
+}
 ${greeting} 🌻🦔`,
   };
 };
